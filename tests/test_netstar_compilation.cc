f8ec338cf736017f693912ee53ef9e4493bde2d5
duanjp8617 <duanjp8617@gmail.com>
2017-10-08 21:38:57 +0800

    sync

--- tests/test_netstar_compilation.cc
+++ tests/test_netstar_compilation.cc
@@ -30,8 +30,13 @@ using namespace seastar;
 int main(int ac, char** av) {
     return app_template().run_deprecated(ac, av, [] {
        printf("Thread %d: In the reactor loop\n", engine().cpu_id());
-       auto ptr = netstar::create_netstar_dpdk_net_device();
+
+       auto ptr = netstar::create_netstar_dpdk_net_device(0, 4);
        printf("Thread %d: netstar_dpdk_device is created\n", engine().cpu_id());
+
+       auto ptr = netstar::create_netstar_dpdk_net_device(1, 4);
+       printf("Thread %d: netstar_dpdk_device is created\n", engine().cpu_id());
+
        engine().exit(0);
     });
 }
