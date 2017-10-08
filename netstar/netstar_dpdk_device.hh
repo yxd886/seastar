94b410135e3dee1dd6ccbfa29d029d6b4ade4727
duanjp8617 <duanjp8617@gmail.com>
2017-10-08 18:22:32 +0800

    sync

--- netstar/netstar_dpdk_device.hh
+++ netstar/netstar_dpdk_device.hh
@@ -13,7 +13,7 @@
 
 namespace netstar{
 
-std::unique_ptr<net::device> create_netstar_dpdk_net_device(
+std::unique_ptr<seastar::net::device> create_netstar_dpdk_net_device(
                                     uint8_t port_idx = 0,
                                     uint8_t num_queues = 1,
                                     bool use_lro = false,
