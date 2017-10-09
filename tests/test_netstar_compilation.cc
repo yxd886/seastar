7dc8db7241c1dbf00808d90b0ae654379e7d42ac
duanjp8617 <duanjp8617@gmail.com>
2017-10-09 16:50:44 +0800

    sync

--- tests/test_netstar_compilation.cc
+++ tests/test_netstar_compilation.cc
@@ -60,10 +60,42 @@ int main(int ac, char** av) {
            });
        }
 
-       sem->wait(smp::count).then([sdev] {
-           sdev->link_ready().then([sdev] {
-               printf("Finish constructing all the qp\n");
-               engine().exit(0);
+       sem->wait(smp::count).then([opts, sdev] {
+           sdev->link_ready().then([opts] {
+               printf("Create device 0\n");
+
+               auto fst_dev_ptr = netstar::create_netstar_dpdk_net_device(1, smp::count);
+               printf("Thread %d: netstar_dpdk_device is created\n", engine().cpu_id());
+
+               auto sem = std::make_shared<semaphore>(0);
+               std::shared_ptr<net::device> sdev(fst_dev_ptr.release());
+
+               for (unsigned i = 0; i < smp::count; i++) {
+                  smp::submit_to(i, [opts, sdev] {
+                      uint16_t qid = engine().cpu_id();
+                      if (qid < sdev->hw_queues_count()) {
+                          auto qp = sdev->init_local_queue(opts, qid);
+                          std::map<unsigned, float> cpu_weights;
+                          for (unsigned i = sdev->hw_queues_count() + qid % sdev->hw_queues_count(); i < smp::count; i+= sdev->hw_queues_count()) {
+                              cpu_weights[i] = 1;
+                          }
+                          cpu_weights[qid] = opts["hw-queue-weight"].as<float>();
+                          qp->configure_proxies(cpu_weights);
+                          sdev->set_local_queue(std::move(qp));
+                      } else {
+                          abort();
+                      }
+                  }).then([sem] {
+                      sem->signal();
+                  });
+               }
+
+               sem->wait(smp::count).then([sdev] {
+                   sdev->link_ready().then([] {
+                       printf("Create device 1\n");
+                       engine().exit(0);
+                   });
+               });
            });
        });
 
