3599b6121e9de7a685be472447d4e20b35f73e14
duanjp8617 <duanjp8617@gmail.com>
2017-10-11 19:41:58 +0800

    sync

--- tests/test_netstar_compilation.cc
+++ tests/test_netstar_compilation.cc
@@ -130,12 +130,12 @@ int main(int ac, char** av) {
            });
        });*/
         auto server = std::make_unique<distributed<work_unit<tester>>>();
-        auto server_ptr = server.release();
-        server->start().then([server_ptr] () {
-            std::unique_ptr<distributed<work_unit<tester>>> server(server_ptr);
-            engine().at_exit([server = std::move(server)] () mutable {
-                return server->stop();
-            });
+        // auto server_ptr = server.release();
+        server->start().then([server=std::move(server)] () mutable {
+            // std::unique_ptr<distributed<work_unit<tester>>> server(server_ptr);
+            // engine().at_exit([server = std::move(server)] () mutable {
+            //     return server->stop();
+            // });
             return server->invoke_on_all([](work_unit<tester>& local_inst){
                 local_inst.get_impl()->call(1);
             });
