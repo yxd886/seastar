16c0355b32553206587302d3770d6bb36f56686b
duanjp8617 <duanjp8617@gmail.com>
2017-10-16 16:19:11 +0800

    sync

--- netstar/work_unit.hh
+++ netstar/work_unit.hh
@@ -6,13 +6,13 @@
 namespace netstar{
 
 class work_unit{
-    std::vector<port&> _all_ports;
+    std::vector<port*> _all_ports;
 protected:
     virtual future<> receive_from_port(uint16_t port_id, net::packet pkt) = 0;
 public:
     void add_port(per_core_objs<port>& ports){
         auto local_port = ports.local_obj();
-        _all_ports.push_back(std::ref(local_port));
+        _all_ports.push_back(&local_port);
         auto port_id = _all_ports.size()-1;
 
         local_port.receive([this, port_id](net::packet pkt){
