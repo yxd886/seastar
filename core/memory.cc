5b3449adc1b374f7a18b7499f2c51f4087c98873
duanjp8617 <duanjp8617@gmail.com>
2017-10-17 14:15:30 +0800

    sync

--- core/memory.cc
+++ core/memory.cc
@@ -1354,6 +1354,7 @@ void configure(std::vector<resource::memory> m, bool mbind,
     if (hugetlbfs_path) {
         cpu_mem.init_virt_to_phys_map();
     }
+    printf("Am I here?\n");
 }
 
 statistics stats() {
