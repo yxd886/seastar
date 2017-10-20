9f8410db7924be7f42809f7618afc890f4001bec
duanjp8617 <duanjp8617@gmail.com>
2017-10-20 15:36:58 +0800

    sync

--- netstar/work_unit.hh
+++ netstar/work_unit.hh
@@ -30,9 +30,8 @@ protected:
         printf("WARNING: Siliently drop the received packet.\n");
         return make_ready_future<>();
     }
-    virtual future<> receive_forwarded(unsigned from_core, net::packet pkt) {
+    virtual void receive_forwarded(unsigned from_core, net::packet pkt) {
         printf("WARNING: Siliently drop the received forwarded packet.\n");
-        return make_ready_future<>();
     }
 
     explicit work_unit(per_core_objs<T*>* objs):
