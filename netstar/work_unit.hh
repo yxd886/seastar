b7b63aa1efe74527617e244685fb91cce85171cc
duanjp8617 <duanjp8617@gmail.com>
2017-10-20 14:53:57 +0800

    sync

--- netstar/work_unit.hh
+++ netstar/work_unit.hh
@@ -4,7 +4,6 @@
 #include <experimental/optional>
 #include "core/future.hh"
 #include "port.hh"
-#include <boost/iterator/counting_iterator.hpp>
 
 namespace netstar{
 
@@ -15,6 +14,7 @@ class work_unit{
     using sub = subscription<net::packet>;
     using optional = std::experimental::optional;
 
+    unsigned _send_queue_length;
     per_core_objs<T>* _all_objs;
     std::vector<port*> _all_ports;
     std::vector<optional<sub>> _all_subs;
@@ -26,10 +26,15 @@ protected:
         return _all_objs;
     }
 
-    virtual future<> receive_from_port(uint16_t port_id, net::packet pkt) = 0;
-    virtual future<> receive_forwarded(unsigned from_core, net::packet pkt) = 0;
+    virtual future<> receive_from_port(uint16_t port_id, net::packet pkt) {
+        printf("WARNING: Siliently drop the received packet.\n");
+    }
+    virtual future<> receive_forwarded(unsigned from_core, net::packet pkt) {
+        printf("WARNING: Siliently drop the received forwarded packet.\n");
+    }
 
-    explicit work_unit(per_core_objs<T*>* objs): _all_objs(objs) {}
+    explicit work_unit(per_core_objs<T*>* objs):
+            _send_queue_length(0), _all_objs(objs) {}
 public:
     void configure_ports(ports_env& env, unsigned first_pos, unsigned last_pos){
         assert(first_pos<last_pos && last_pos<env.count() && _all_ports.size() == 0);
@@ -65,9 +70,14 @@ public:
         return (*_all_ports.at(port_id)).send(std::move(pkt));
     }
 
-    /*template<typename T>
-    inline void forward_to(per_core_objs<T>* work_units){
-        static_assert(std::is_base_of<work_unit, B>::value)
+    /*inline future<> forward_to(unsigned dst_core, net::packet pkt){
+        auto src_core = engine().cpu_id();
+        auto& peer = _all_objs->get_obj(dst_core);
+        _send_queue_length += 1;
+
+        smp::submit_to(dst_core, [this, &peer, src_core, pkt = std::move(pkt)] () mutable {
+           peer.receive_forwarded(src_core, pkt.free_on_cpu(src_core, [this]{_send_queue_length -= 1;}));
+        });
     }*/
 };
 
