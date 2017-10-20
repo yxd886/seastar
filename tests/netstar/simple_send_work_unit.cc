2d4f9280f03353db5caeb55edee294639b66c5ed
duanjp8617 <duanjp8617@gmail.com>
2017-10-20 16:33:34 +0800

    sync

--- tests/netstar/simple_send_work_unit.cc
+++ tests/netstar/simple_send_work_unit.cc
@@ -26,6 +26,12 @@
 #include "netstar/netstar_dpdk_device.hh"
 #include "netstar/port.hh"
 #include "netstar/work_unit.hh"
+#include "net/udp.hh"
+#include "net/ip_checksum.hh"
+#include "net/ip.hh"
+#include "net/net.hh"
+#include "net/packet.hh"
+#include "net/byteorder.hh"
 
 using namespace seastar;
 using namespace netstar;
@@ -42,6 +48,87 @@ public:
 
     future<> stop(){
         return make_ready_future<>();
+    }
+
+    void send_from_port_0(){
+        _stats_timer.set_callback([this] {
+            std::cout << "Out: " << _n_sent << " pps, \t";
+            std::cout << "Err: " << _n_failed << " pps, \t";
+            std::cout << "In: " << _n_received << " pps" << std::endl;
+            _n_sent = 0;
+            _n_received = 0;
+            _n_failed = 0;
+        });
+        _stats_timer.arm_periodic(1s);
+        _pkt = build_pkt(
+                "aaaaaaaaaaaaaaaaaaaaa");
+
+        keep_doing([this](){
+           net::packet pkt(_pkt.frag(0));
+           auto len = pkt.len();
+
+           return this->send_from_port(0, std::move(pkt));
+        });
+    }
+
+private:
+    uint64_t _n_sent {};
+    uint64_t _n_received {};
+    uint64_t _n_failed {};
+    timer<> _stats_timer;
+    net::packet _pkt;
+    net::packet build_pkt(const char* buf){
+        ipv4_addr ipv4_src_addr("10.1.2.4:666");
+        ipv4_addr ipv4_dst_addr("10.1.2.5:666");
+        auto pkt = net::packet::from_static_data(buf, strlen(buf));
+
+        auto hdr = pkt.prepend_header<net::udp_hdr>();
+        hdr->src_port = ipv4_src_addr.port;
+        hdr->dst_port = ipv4_dst_addr.port;
+        hdr->len = pkt.len();
+        *hdr = net::hton(*hdr);
+
+        net::checksummer csum;
+        net::ipv4_traits::udp_pseudo_header_checksum(csum, ipv4_src_addr, ipv4_dst_addr, pkt.len());
+        csum.sum(pkt);
+        hdr->cksum = csum.get();
+
+        net::offload_info oi;
+        oi.needs_csum = false;
+        oi.protocol = net::ip_protocol_num::udp;
+        pkt.set_offload_info(oi);
+
+        auto iph = pkt.prepend_header<net::ip_hdr>();
+        iph->ihl = sizeof(*iph) / 4;
+        iph->ver = 4;
+        iph->dscp = 0;
+        iph->ecn = 0;
+        iph->len = pkt.len();
+        iph->id = 0;
+        iph->frag = 0;
+        iph->ttl = 64;
+        iph->ip_proto = (uint8_t)net::ip_protocol_num::udp;
+        iph->csum = 0;
+        iph->src_ip = net::ipv4_address(ipv4_src_addr.ip);
+        iph->dst_ip = net::ipv4_address(ipv4_dst_addr.ip);
+        *iph = net::hton(*iph);
+        net::checksummer ip_csum;
+        ip_csum.sum(reinterpret_cast<char*>(iph), sizeof(*iph));
+        iph->csum = csum.get();
+
+        auto eh = pkt.prepend_header<net::eth_hdr>();
+        net::ethernet_address eth_src{0x52, 0x54, 0x00, 0xfe, 0x22, 0x11};
+        net::ethernet_address eth_dst{0x52, 0x54, 0x00, 0xfe, 0x22, 0x42};
+        eh->dst_mac = eth_dst;
+        eh->src_mac = eth_src;
+        eh->eth_proto = uint16_t(net::eth_protocol_num::ipv4);
+        *eh = net::hton(*eh);
+
+        pkt.linearize();
+        std::cout<< "pkt.nr_framgs() = " << pkt.nr_frags() <<std::endl;
+        std::cout<<"pkt.len() = "<< pkt.len() <<std::endl;
+        assert(pkt.nr_frags() == 1);
+        return pkt;
     }
 };
 
