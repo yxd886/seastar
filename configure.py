1ddb0a7982857e431b82e5cc932643ff4d20df85
duanjp8617 <duanjp8617@gmail.com>
2017-10-23 16:02:59 +0800

    sync

--- configure.py
+++ configure.py
@@ -341,11 +341,6 @@ libnet = [
 libnetstar = [
     'netstar/netstar_dpdk_device.cc',
     'netstar/port.cc',
-    'netstar/mica/util/config.cc',
-	'netstar/mica/util/cityhash/city_mod.cc',
-	'netstar/mica/util/lcore.cc',
-	'netstar/mica/util/stopwatch.cc',
-	'netstar/mica/util/zipf.cc'
     ]
     
 micautil = [
