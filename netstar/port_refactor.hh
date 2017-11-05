ef1cef845b4b9da52c5f25a14345003af4e3dc26
duanjp8617 <duanjp8617@gmail.com>
2017-11-05 21:10:18 +0800

    sync

--- netstar/port_refactor.hh
+++ netstar/port_refactor.hh
@@ -10,6 +10,7 @@
 #include "per_core_objs.hh"
 #include "core/semaphore.hh"
 #include "core/shared_ptr.hh"
+#include "netstar/qp_wrapper.hh"
 
 using namespace seastar;
 
@@ -21,81 +22,6 @@ enum class port_type{
     original,
     netstar_dpdk,
     fdir
-};
-
-// This is a wrapper class to net::qp.
-// The most important functionality of this class
-// is to store a unique_ptr to net::qp.
-// It also provides several public interfaces for
-// accessing net::qp.
-class qp_wrapper{
-    uint16_t _qid;
-    net::device* _dev;
-    std::unique_ptr<net::qp> _qp;
-public:
-    // The default constructor.
-    explicit qp_wrapper(boost::program_options::variables_map opts,
-                         net::device* dev,
-                         uint16_t qid) :
-                         _qid(qid), _dev(dev){
-        if(_qid < _dev->hw_queues_count()){
-            _qp = _dev->init_local_queue(opts, _qid);
-
-            std::map<unsigned, float> cpu_weights;
-            for (unsigned i = _dev->hw_queues_count() + _qid % _dev->hw_queues_count();
-                 i < smp::count;
-                 i+= _dev->hw_queues_count()) {
-                cpu_weights[i] = 1;
-            }
-            cpu_weights[_qid] = opts["hw-queue-weight"].as<float>();
-            _qp->configure_proxies(cpu_weights);
-
-            _dev->update_local_queue(_qp.get());
-        }
-        else{
-            auto master = _qid % _dev->hw_queues_count();
-            _qp = create_proxy_net_device(master, _dev);
-
-            _dev->update_local_queue(_qp.get());
-        }
-    }
-
-    // Register packet provider for the qp
-    void register_packet_provider(std::function<std::experimental::optional<net::packet>()> fn){
-        _qp->register_packet_provider(std::move(fn));
-    }
-
-    // Start the rx_stream of qp
-    subscription<net::packet>
-    receive(std::function<future<> (net::packet)> next_packet) {
-        return _dev->receive(std::move(next_packet));
-    }
-
-public:
-    // Utility functions exported by port_wrapper:
-
-    // Obtain the default ethernet address of the underlying NIC
-    net::ethernet_address get_eth_addr(){
-        return _dev->hw_address();
-    }
-    // Obtain the configured RS key.
-    const rss_key_type& get_rss_key(){
-        return _dev->rss_key();
-    }
-    // Giving an RSS key, return which CPU is this RSS key
-    // mapped to.
-    unsigned hash2cpu(uint32_t hash){
-        return _dev->hash2qid(hash);
-    }
-    // Obtain the queue id of the qp
-    unsigned get_qid(){
-        return _qid;
-    }
-    // Obtain the number of hardware queues configured
-    // for the underlying DPDK device
-    uint16_t get_hw_queues_count(){
-        return _dev->hw_queues_count();
-    }
 };
 
 class port{
