41145fc437b3a2dd6ee9640986160f1a9823148a
duanjp8617 <duanjp8617@gmail.com>
2017-11-06 21:52:27 +0800

    sync

--- net/net.cc
+++ net/net.cc
@@ -259,7 +259,6 @@ interface::interface(std::shared_ptr<device> dev)
             }
             return p;
         });
-    printf("Thread %d: finish construcint interface\n", engine().cpu_id());
 }
 
 subscription<packet, ethernet_address>
