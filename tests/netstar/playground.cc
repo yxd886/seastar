73b6d3f6c1e5b94567366fd05d62114706182781
duanjp8617 <duanjp8617@gmail.com>
2017-11-06 21:03:08 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -37,83 +37,17 @@ using namespace netstar;
 int main(int ac, char** av) {
     app_template app;
     ports_env all_ports;
-    per_core_objs<mica_client> all_objs;
-    vector<vector<port_pair>> queue_map;
 
-    return app.run_deprecated(ac, av, [&app, &all_ports, &all_objs, &queue_map]{
+    return app.run_deprecated(ac, av, [&app, &all_ports]{
         auto& opts = app.configuration();
+        std::unordered_map<std::string, net::ipv4_address> p0_addr_map;
+        p0_addr_map["host-ipv4-addr"] = net::ipv4_address("10.28.1.13");
+        p0_addr_map["gw-ipv4-addr"] = net::ipv4_address("10.28.1.1");
+        p0_addr_map["netmask-ipv4-addr"] = net::ipv4_address("255.255.255.255");
 
-        return all_ports.add_port(opts, 1, smp::count, port_type::fdir).then([&all_objs]{
-            return all_objs.start(&all_objs);
-        }).then([&all_ports, &all_objs]{
-            return all_objs.invoke_on_all([&all_ports](mica_client& mc){
-                mc.configure_ports(all_ports, 0, 0);
-            });
-        }).then([&opts, &all_ports, &queue_map]{
-            queue_map = calculate_queue_mapping(
-                    opts, all_ports.local_port(0));
-        }).then([&all_objs, &opts, &queue_map]{
-            return all_objs.invoke_on_all([&opts, &queue_map](mica_client& mc){
-                mc.bootup(opts, queue_map);
-            });
-        }).then([&all_objs]{
-            return all_objs.invoke_on_all([](mica_client& mc){
-                mc.start_receiving();
-            });
-        }).then([&all_objs]{
-            return smp::submit_to(3, [&all_objs]{
-                uint64_t key = 10276325;
-                extendable_buffer key_buf;
-                key_buf.fill_data(key);
-
-                uint64_t val = 8721;
-                extendable_buffer val_buf;
-                val_buf.fill_data(val);
-
-                return all_objs.local_obj().query(Operation::kSet,
-                        sizeof(uint64_t), key_buf.get_temp_buffer(),
-                        sizeof(uint64_t), val_buf.get_temp_buffer()).then_wrapped([](auto&& f){
-                    try{
-                        auto response = std::get<0>(f.get());
-                        printf("No error!!!!\n");
-                        auto op = static_cast<uint8_t>(response.get_operation());
-                        auto r = static_cast<uint8_t>(response.get_result());
-                        printf("Operation %d, result %d\n", op, r);
-                        auto key_len = response.get_key_len();
-                        auto val_len = response.get_val_len();
-                        std::cout<<"key_len "<<key_len<<" val_len "<<val_len<<std::endl;
-                    }
-                    catch(...){
-                        printf("We got some errors here!\n");
-                    }
-                });
-            });
-        }).then([&all_objs]{
-            return smp::submit_to(3, [&all_objs]{
-                uint64_t key = 10276325;
-                extendable_buffer key_buf;
-                key_buf.fill_data(key);
-
-                return all_objs.local_obj().query(Operation::kGet,
-                        sizeof(uint64_t), key_buf.get_temp_buffer(),
-                        0, temporary_buffer<char>{}).then_wrapped([](auto&& f){
-                    try{
-                        auto response = std::get<0>(f.get());
-                        printf("No error!!!!\n");
-                        auto op = static_cast<uint8_t>(response.get_operation());
-                        auto r = static_cast<uint8_t>(response.get_result());
-                        printf("Operation %d, result %d\n", op, r);
-                        auto key_len = response.get_key_len();
-                        auto val_len = response.get_val_len();
-                        std::cout<<"key_len "<<key_len<<" val_len "<<val_len<<std::endl;
-                    }
-                    catch(...){
-                        printf("We got some errors here!\n");
-                    }
-                });
-            });
-        }).then([]{
-            printf("The mica client is successfully booted up\n");
+        return all_ports.add_stack_port(opts, 0, smp::count, std::move(p0_addr_map)).then([]{
+            printf("Finish creating two stack ports\n");
+            engine().exit(0);
         });
     });
 }
