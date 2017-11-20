2300f3042d58a013d94705f2e529b4ca5d6403ce
yxd886 <409070169@qq.com>
2017-11-20 16:26:03 +0800

    a

--- configure.py
+++ configure.py
@@ -455,7 +455,7 @@ deps = {
     'tests/foreign_ptr_test': ['tests/foreign_ptr_test.cc'] + core,
     'tests/semaphore_test': ['tests/semaphore_test.cc'] + core,
     'tests/expiring_fifo_test': ['tests/expiring_fifo_test.cc'] + core,
-    'tests/smp_test': ['tests/smp_test.cc'] + core +aho_corasick,
+    'tests/smp_test': ['tests/smp_test.cc'] + core,
     'tests/thread_test': ['tests/thread_test.cc'] + core,
     'tests/thread_context_switch': ['tests/thread_context_switch.cc'] + core,
     'tests/udp_server': ['tests/udp_server.cc'] + core + libnet,
