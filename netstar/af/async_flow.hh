4c5bc65b4673342a30d487a74fc6f20c790d7935
duanjp8617 <duanjp8617@gmail.com>
2017-12-01 14:19:57 +0800

    update async_flow.hh

--- netstar/af/async_flow.hh
+++ netstar/af/async_flow.hh
@@ -205,6 +205,9 @@ public:
         if(!working_unit.flow_key) {
             async_flow_assert(!is_client);
             FlowKeyType flow_key = working_unit.ppr.get_reverse_flow_key(pkt);
+            if(_manager.add_new_mapping_to_flow_table(flow_key, )) {
+
+            }
         }
 
         action_after_packet_handle(working_unit, std::move(pkt),
