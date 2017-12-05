a7fbb4045c0b7a1e8fb77b18ddce62753beac8be
duanjp8617 <duanjp8617@gmail.com>
2017-12-05 14:35:05 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -162,5 +162,7 @@ int main(int ac, char** av) {
         ingress.register_to_manager(manager, [](net::packet pkt){return make_ready_future();}, egress);
         egress.register_to_manager(manager, [](net::packet pkt){return make_ready_future();}, ingress);
         net::packet pkt(the_pkt.frag(0));
+        dummy_udp_ppr::FlowKeyType fk = dummy_udp_ppr::async_flow_config::get_flow_key(pkt);
+        ingress.get_send_stream().produce(std::move(pkt), fk);
     });
 }
