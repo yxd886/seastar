9161f375a9f05586534ed9cb3eabc857152a2ed6
duanjp8617 <duanjp8617@gmail.com>
2017-12-06 20:32:50 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -167,6 +167,7 @@ public:
 
 struct dummy{
     int i;
+    dummy(int i_arg) : i(i_arg){}
     ~dummy(){
         printf("dummy is deconstructed\n");
     }
@@ -180,7 +181,7 @@ int main(int ac, char** av) {
     async_flow_manager<dummy_udp_ppr>::external_io_direction egress(1);
     net::packet the_pkt = dummy_udp_ppr::async_flow_config::build_pkt("abcdefg");
     std::experimental::optional<dummy> d;
-    d = dummy{1};
+    d.emplace(1);
     d = {};
 
     return app.run_deprecated(ac, av, [&app, &to, &manager, &ingress, &egress, &the_pkt]{
