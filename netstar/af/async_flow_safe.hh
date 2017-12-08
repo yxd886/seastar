fa666f527ff276682d7132ec326ac25665028870
duanjp8617 <duanjp8617@gmail.com>
2017-12-08 20:45:12 +0800

    sync

--- netstar/af/async_flow_safe.hh
+++ netstar/af/async_flow_safe.hh
@@ -1,6 +1,8 @@
 #ifndef _ASYNC_FLOW_SAFE_HH
 #define _ASYNC_FLOW_SAFE_HH
 
+#include "core/gate.hh"
+
 #include "netstar/af/async_flow.hh"
 
 using namespace seastar;
@@ -11,12 +13,16 @@ template<typename Ppr>
 class async_flow_safe {
     lw_shared_ptr<client_async_flow<Ppr>> _client;
     lw_shared_ptr<server_async_flow<Ppr>> _server;
+    lw_shared_ptr<gate> _g;
 
     async_flow_safe(client_async_flow<Ppr>&& client,
                     server_async_flow<Ppr>&& server)
         : _client(make_lw_shared(std::move(client)))
-        , _server(make_lw_shared(std::move(server))){
+        , _server(make_lw_shared(std::move(server)))
+        , _g(make_lw_shared(gate())){
     }
+
+
 };
 
 }
