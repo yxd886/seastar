96c2e918339f5ba5b0f481d76c342769e1ca128a
duanjp8617 <duanjp8617@gmail.com>
2017-12-12 12:32:02 +0800

    sync

--- net/tcp.hh
+++ net/tcp.hh
@@ -792,16 +792,22 @@ auto tcp<InetTraits>::connect(socket_address sa) -> connection {
     auto dst_ip = ipv4_address(sa);
     auto dst_port = net::ntoh(sa.u.in.sin_port);
 
-    do {
+    auto netif = _inet._inet.netif();
+    while(true) {
         src_port = _port_dist(_e);
         id = connid{src_ip, dst_ip, src_port, dst_port};
-    } while( (_tcbs.find(id) != _tcbs.end()) &&
-             (_inet._inet.netif()->hw_queues_count() == 1 ||
-              _inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()) );
-
-              /*( (_inet._inet.netif()->hw_queues_count() > 1 || _tcbs.find(id) != _tcbs.end()) &&
-                (_inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()
-                || _tcbs.find(id) != _tcbs.end()) ) */ ;
+        if(netif->hw_queues_count() > 1) {
+            if( netif->hash2cpu(id.hash(netif->rss_key())) == engine().cpu_id()
+                && _tcbs.find(id) == _tcbs.end() ){
+                break;
+            }
+        }
+        else{
+            if(_tcbs.find(id) == _tcbs.end()){
+                break;
+            }
+        }
+    }
 
     auto tcbp = make_lw_shared<tcb>(*this, id);
     _tcbs.insert({id, tcbp});
