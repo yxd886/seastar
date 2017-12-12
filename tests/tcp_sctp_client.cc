6b03c10ec85d913096365b5fa4684b6e89d8170b
duanjp8617 <duanjp8617@gmail.com>
2017-12-12 16:41:27 +0800

    sync

--- tests/tcp_sctp_client.cc
+++ tests/tcp_sctp_client.cc
@@ -328,7 +328,7 @@ int main(int ac, char ** av) {
         auto proto = config["proto"].as<std::string>();
         auto time = config["time"].as<size_t>();
 
-        size_t total_transmission_bytes = 10*1024*1024*1024*time/8;
+        size_t total_transmission_bytes = static_cast<size_t>(1024*1024*1024*10)*time/static_cast<size_t>(8);
         size_t per_connection_transmission_bytes = total_transmission_bytes/static_cast<size_t>((ncon*smp::count));
         tx_msg_nr = per_connection_transmission_bytes/tx_msg_size + 1;
 
