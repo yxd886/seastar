98509539fd3f8e5c393e78565877a0ffddac7616
duanjp8617 <duanjp8617@gmail.com>
2017-12-12 11:43:21 +0800

    sync

--- net/tcp.hh
+++ net/tcp.hh
@@ -807,9 +807,9 @@ auto tcp<InetTraits>::connect(socket_address sa) -> connection {
     do {
         src_port = _port_dist(_e);
         id = connid{src_ip, dst_ip, src_port, dst_port};
-    } while( (_tcbs.find(id) != _tcbs.end()) &&
-             (_inet._inet.netif()->hw_queues_count() == 1 ||
-              _inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()) );
+    } while( (_inet._inet.netif()->hw_queues_count() == 1 ||
+              _inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()) &&
+             (_tcbs.find(id) != _tcbs.end()) );
 
 
     auto tcbp = make_lw_shared<tcb>(*this, id);
