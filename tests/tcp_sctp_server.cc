543eb70b749c1798c887cd5e112d1cb9d25731bb
duanjp8617 <duanjp8617@gmail.com>
2017-12-13 11:42:28 +0800

    sync

--- tests/tcp_sctp_server.cc
+++ tests/tcp_sctp_server.cc
@@ -94,6 +94,10 @@ public:
         connected_socket _fd;
         input_stream<char> _read_buf;
         output_stream<char> _write_buf;
+        size_t _received_bytes = 0;
+
+        timer<lowres_clock> _reporter;
+        size_t _snap_shot = 0;
     public:
         connection(tcp_server& server, connected_socket&& fd, socket_address addr)
             : _fd(std::move(fd))
@@ -158,11 +162,20 @@ public:
                 if (buf.size() == 0) {
                     return make_ready_future();
                 } else {
+                    _received_bytes += buf.size();
                     return do_read();
                 }
             });
         }
         future<> rx_test() {
+            _snap_shot = _received_bytes;
+            _reporter.set_callback([this](){
+                if(_snap_shot == _received_bytes) {
+                    _fd.dump_tcp();
+                }
+                _snap_shot = _received_bytes;
+            });
+
             return do_read().then([] {
                 return make_ready_future<>();
             });
