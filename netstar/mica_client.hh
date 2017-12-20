80643f16d05df8f7a00fcaadb6a5604ee9bcaf9b
duanjp8617 <duanjp8617@gmail.com>
2017-12-20 19:46:20 +0800

    sync

--- netstar/mica_client.hh
+++ netstar/mica_client.hh
@@ -143,7 +143,7 @@ public:
         size_t _request_size;
 
         // maximum number of allowed timeout retries
-        static constexpr unsigned max_retries = 2;
+        static constexpr unsigned max_retries = 5;
 
         // Initial timeout time in millisecond
         static constexpr unsigned initial_timeout_val = 10;
