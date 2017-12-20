64d56d8114089740e138bb655204569d8682a439
duanjp8617 <duanjp8617@gmail.com>
2017-12-20 23:37:31 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -286,7 +286,7 @@ public:
                         auto src_ip = ac.get_src_ip();
                         extendable_buffer key_buf;
                         key_buf.fill_data(src_ip);
-                        return this->_mc.query(Operation::kGet, sizeof(src_ip), std::move(key_buf),
+                        return this->_mc.query(Operation::kGet, sizeof(src_ip), key_buf.get_temp_buffer(),
                                                0, temporary_buffer<char>()).then([&ac, this](mica_response response){
                             auto src_ip = ac.get_src_ip();
                             extendable_buffer key_buf;
@@ -299,7 +299,7 @@ public:
                                 val_buf.fill_data(val);
 
                                 return this->_mc.query(Operation::kSet,
-                                        sizeof(src_ip), std::move(key_buf),
+                                        sizeof(src_ip), key_buf.get_temp_buffer(),
                                         sizeof(val), val_buf.get_temp_buffer());
                             }
                             else{
@@ -309,7 +309,7 @@ public:
                                 val_buf.fill_data(val);
 
                                 return this->_mc.query(Operation::kSet,
-                                                       sizeof(src_ip), std::move(key_buf),
+                                                       sizeof(src_ip), key_buf.get_temp_buffer(),
                                                        sizeof(val), val_buf.get_temp_buffer());
                             }
                         }).then_wrapped([&ac, this](auto&& f){
