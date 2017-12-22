82a3651b3495863fa9339f082be6e6b37d2fee35
duanjp8617 <duanjp8617@gmail.com>
2017-12-22 15:35:12 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -348,6 +348,37 @@ public:
                                                            sizeof(val), val_buf.get_temp_buffer());
                                 }
                             });
+                        }).then([&ac, this](mica_response response){
+                            auto src_ip = wtf{ac.get_flow_key_hash(), ac.get_flow_key_hash()};
+                            extendable_buffer key_buf;
+                            key_buf.fill_data(src_ip);
+                            return this->_mc.query(Operation::kGet, sizeof(src_ip), key_buf.get_temp_buffer(),
+                                                   0, temporary_buffer<char>()).then([&ac, this](mica_response response){
+                                auto src_ip = wtf{ac.get_flow_key_hash(), ac.get_flow_key_hash()};
+                                extendable_buffer key_buf;
+                                key_buf.fill_data(src_ip);
+
+                                if(response.get_result() == Result::kNotFound) {
+                                    // fprint(std::cout,"Key does not exist.\n");
+                                    uint64_t val;
+                                    extendable_buffer val_buf;
+                                    val_buf.fill_data(val);
+
+                                    return this->_mc.query(Operation::kSet,
+                                            sizeof(src_ip), key_buf.get_temp_buffer(),
+                                            sizeof(val), val_buf.get_temp_buffer());
+                                }
+                                else{
+                                    // fprint(std::cout,"Key exist.\n");
+                                    uint64_t val;
+                                    extendable_buffer val_buf;
+                                    val_buf.fill_data(val);
+
+                                    return this->_mc.query(Operation::kSet,
+                                                           sizeof(src_ip), key_buf.get_temp_buffer(),
+                                                           sizeof(val), val_buf.get_temp_buffer());
+                                }
+                            });
                         }).then_wrapped([&ac, this](auto&& f){
                             try{
                                 f.get();
@@ -485,3 +516,5 @@ int main(int ac, char** av) {
 
 // 1r: 8.5M.
 // 1r1w: 5.35M
+// 2r2w: 3.4M
+// 3r3w:
