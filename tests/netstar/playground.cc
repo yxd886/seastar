4813ee5db566870334170cceaf82c97f4a917744
duanjp8617 <duanjp8617@gmail.com>
2017-12-22 16:55:54 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -288,12 +288,12 @@ public:
                             return make_ready_future<af_action>(af_action::close_forward);
                         }
 
-                        auto src_ip = wtf{ac.get_flow_key_hash(), ac.get_flow_key_hash()};
+                        auto src_ip = ac.get_flow_key_hash();
                         extendable_buffer key_buf;
                         key_buf.fill_data(src_ip);
                         return this->_mc.query(Operation::kGet, sizeof(src_ip), key_buf.get_temp_buffer(),
                                                0, temporary_buffer<char>()).then([&ac, this](mica_response response){
-                            auto src_ip = wtf{ac.get_flow_key_hash(), ac.get_flow_key_hash()};
+                            auto src_ip = ac.get_flow_key_hash();
                             extendable_buffer key_buf;
                             key_buf.fill_data(src_ip);
 
