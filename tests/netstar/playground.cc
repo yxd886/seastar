fab25e94c724745b9e5424ee53263b0c78bc95cf
duanjp8617 <duanjp8617@gmail.com>
2018-01-07 00:15:47 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -19,6 +19,9 @@
  * Copyright (C) 2014 Cloudius Systems, Ltd.
  */
 
+
+#include "nf/firewall.hh"
+
 #include "core/reactor.hh"
 #include "core/app-template.hh"
 #include "core/print.hh"
@@ -36,12 +39,17 @@
 #include "netstar/work_unit.hh"
 #include "netstar/port_env.hh"
 #include "netstar/af/cb_async_flow.hh"
+#include "netstar/mica_client.hh"
+#include "netstar/extendable_buffer.hh"
 
 #include "bess/bess_flow_gen.hh"
+
+#include <vector>
 
 using namespace seastar;
 using namespace netstar;
 using namespace std::chrono_literals;
+using std::vector;
 
 enum class dummy_udp_events : uint8_t{
     pkt_in=0
@@ -112,6 +120,7 @@ public:
 };
 
 class forwarder;
+
 distributed<forwarder> forwarders;
 
 class forwarder {
@@ -126,16 +135,75 @@ class forwarder {
     cb_async_flow_manager<dummy_udp_ppr>::external_io_direction _udp_manager_ingress;
     cb_async_flow_manager<dummy_udp_ppr>::external_io_direction _udp_manager_egress;
 
+    mica_client& _mc;
 public:
-    forwarder (ports_env& all_ports)
+    forwarder (ports_env& all_ports, per_core_objs<mica_client>& mica_clients)
         : _ingress_port(std::ref(all_ports.local_port(0)))
         , _egress_port(std::ref(all_ports.local_port(1)))
         , _udp_manager_ingress(0)
-        , _udp_manager_egress(1){
+        , _udp_manager_egress(1)
+        , _mc(std::ref(mica_clients.local_obj())){
     }
 
     future<> stop(){
         return make_ready_future<>();
+    }
+
+    future<> mica_test(int ) {
+        // only test mica performance on thread 1.
+        return repeat([this]{
+            uint64_t key = engine().cpu_id()+1;
+            extendable_buffer key_buf;
+            key_buf.fill_data(key);
+
+            return _mc.query(Operation::kGet,sizeof(key), key_buf.get_temp_buffer(),
+                             0, temporary_buffer<char>()).then([this](mica_response response){
+                assert(response.get_result() == Result::kNotFound);
+
+                uint64_t key = engine().cpu_id()+1;
+                extendable_buffer key_buf;
+                key_buf.fill_data(key);
+
+                uint64_t val = 6;
+                extendable_buffer val_buf;
+                val_buf.fill_data(val);
+                return _mc.query(Operation::kSet, sizeof(key), key_buf.get_temp_buffer(),
+                                 sizeof(val), val_buf.get_temp_buffer());
+            }).then([this](mica_response response){
+                assert(response.get_result() == Result::kSuccess);
+
+                uint64_t key = engine().cpu_id()+1;
+                extendable_buffer key_buf;
+                key_buf.fill_data(key);
+
+                return _mc.query(Operation::kGet,sizeof(key), key_buf.get_temp_buffer(),
+                                 0, temporary_buffer<char>());
+            }).then([this](mica_response response){
+                assert(response.get_value<uint64_t>() == 6);
+
+                uint64_t key = engine().cpu_id()+1;
+                extendable_buffer key_buf;
+                key_buf.fill_data(key);
+
+                return _mc.query(Operation::kDelete,
+                                 sizeof(key), key_buf.get_temp_buffer(),
+                                 0, temporary_buffer<char>());
+            }).then([this](mica_response response){
+                assert(response.get_result() == Result::kSuccess);
+            }).then_wrapped([](auto&& f){
+                try{
+                    f.get();
+                    fprint(std::cout, "mica_test succeeds on core %d!\n", engine().cpu_id());
+                    return make_ready_future<stop_iteration>(stop_iteration::yes);
+                }
+                catch(...) {
+                    fprint(std::cout, "mica_test fails on core %d, retry in 3s.\n", engine().cpu_id());
+                    return sleep(3s).then([]{
+                        return stop_iteration::no;
+                    });
+                }
+            });
+        });
     }
 
     void configure(int i) {
@@ -145,7 +213,7 @@ public:
             auto eth_h = pkt.get_header<net::eth_hdr>(0);
             eth_h->src_mac = net::ethernet_address{0x3c, 0xfd, 0xfe, 0x06, 0x09, 0x62};
             eth_h->dst_mac = net::ethernet_address{0x3c, 0xfd, 0xfe, 0x06, 0x07, 0x82};
-            _egress_port.send(std::move(pkt));
+            _ingress_port.send(std::move(pkt));
             return make_ready_future<>();
         };
 
@@ -203,34 +271,156 @@ public:
         }));
     }
 
-    class cb_async_flow_shell {
-        cb_async_flow<dummy_udp_ppr> _af;
+    struct firewall_flow_state {
+        uint8_t tcp_flags;
+        uint32_t sent_seq;
+        uint32_t recv_ack;
+        bool pass;
+    };
+
+    struct query_key {
+        uint64_t v1;
+        uint64_t v2;
+    };
+
+    class firewall_runner {
+        cb_async_flow<dummy_udp_ppr> _ac;
+        forwarder& _f;
+        firewall_flow_state _fs;
     public:
-        cb_async_flow_shell(cb_async_flow<dummy_udp_ppr> af)
-            : _af(std::move(af)) {
+        firewall_runner(cb_async_flow<dummy_udp_ppr> ac, forwarder& f)
+            : _ac(std::move(ac))
+            , _f(f){}
+
+        void events_registration() {
+            _ac.register_events(dummy_udp_events::pkt_in);
         }
 
-        void register_event(){
-            _af.register_events(dummy_udp_events::pkt_in);
-        }
-
-        void register_cb(){
-            _af.register_cbs([this]{pkt_cb();}, [this]{close_cb();});
-        }
-
-    private:
-        void pkt_cb() {
-            if(_af.cur_event().on_close_event()) {
-                _af.drop_cur_packet();
-                _af.unregister_packet_cb();
-                return;
-            }
-            _af.forward_cur_packet();
-        }
-        void close_cb(){
+        void close_cb() {
             delete this;
         }
 
+        void db_write_cb(int err_code, mica_response res) {
+            if(err_code!=0){
+                _ac.drop_cur_packet();
+                return;
+            }
+            forward_packet(_fs);
+        }
+
+        void db_read_cb(int err_code, mica_response res) {
+            if(err_code!=0){
+                _ac.drop_cur_packet();
+                return;
+            }
+
+            bool state_changed = true;
+            if(res.get_result() == Result::kNotFound) {
+                initialize_session_state(_ac.cur_packet(), _fs);
+            }
+            else{
+                _fs = res.get_value<firewall_flow_state>();
+               state_changed = update_session_state(_ac.cur_packet(), _fs);
+            }
+
+            if(state_changed) {
+                auto key = query_key{_ac.get_flow_key_hash(), _ac.get_flow_key_hash()};
+                _f._mc.query_with_cb(Operation::kSet, mica_key(key), mica_value(_fs), [this](int err_code, mica_response res){
+                    db_write_cb(err_code, std::move(res));
+                });
+            }
+            else{
+                forward_packet(_fs);
+            }
+        }
+
+        void pkt_cb() {
+            if(_ac.cur_event().on_close_event()) {
+                _ac.drop_cur_packet();
+                _ac.unregister_packet_cb();
+                return;
+            }
+
+            auto key = query_key{_ac.get_flow_key_hash(), _ac.get_flow_key_hash()};
+            _f._mc.query_with_cb(Operation::kGet, mica_key(key),
+                        mica_value(0, temporary_buffer<char>()), [this](int err_code, mica_response res){
+                db_read_cb(err_code, std::move(res));
+            });
+        }
+
+        void run_firewall() {
+            _ac.register_cbs([this]{pkt_cb();}, [this]{close_cb();});
+        }
+
+        /*future<> run_firewall() {
+            return _ac.run_async_loop([this](){
+                if(_ac.cur_event().on_close_event()) {
+                    return make_ready_future<af_action>(af_action::close_forward);
+                }
+                auto key = query_key{_ac.get_flow_key_hash(), _ac.get_flow_key_hash()};
+                return _f._mc.query(Operation::kGet, mica_key(key),
+                        mica_value(0, temporary_buffer<char>())).then([this](mica_response response){
+                    bool state_changed = true;
+                   if(res.get_result() == Result::kNotFound) {
+                       initialize_session_state(_ac.cur_packet(), _fs);
+                   }
+                   else{
+                       _fs = res.get_value<firewall_flow_state>();
+                      state_changed = update_session_state(_ac.cur_packet(), _fs);
+                   }
+
+                   if(state_changed) {
+                       auto key = query_key{_ac.get_flow_key_hash(), _ac.get_flow_key_hash()};
+                       return _f._mc.query(Operation::kSet, mica_key(key),
+                               mica_value(_fs)).then([this](mica_response response){
+                           return forward_packet(_fs);
+                       });
+                   }
+                   else{
+                       forward_packet(_fs);
+                   }
+                }).then_wrapped([this](auto&& f){
+                    try{
+                        auto result = f.get0();
+                        return result;
+
+                    }
+                    catch(...){
+                        return af_action::drop;
+                    }
+                });
+            });
+        }*/
+
+        void initialize_session_state(net::packet& pkt, firewall_flow_state& state) {
+            auto ip_hdr = pkt.get_header<net::ip_hdr>(sizeof(net::eth_hdr));
+            auto udp_hdr = pkt.get_header<net::udp_hdr>(sizeof(net::eth_hdr) + sizeof(net::ip_hdr));
+
+            for(auto& rule : _f.firewall.rules){
+                if(ip_hdr->dst_ip.ip == rule._dst_addr &&
+                   udp_hdr->dst_port == rule._dst_port &&
+                   ip_hdr->src_ip.ip == rule._src_addr &&
+                   udp_hdr->src_port == rule._src_port){
+                    state.pass = false;
+                    return;
+                }
+            }
+
+            state.pass = true;
+        }
+
+        bool update_session_state(net::packet& pkt, firewall_flow_state& state) {
+            return false;
+        }
+
+        void forward_packet(firewall_flow_state& fs) {
+            if(fs.pass){
+                _ac.forward_cur_packet();
+            }
+            else{
+                _ac.drop_cur_packet();
+            }
+        }
     };
 
     void run_udp_manager(int) {
@@ -238,11 +428,9 @@ public:
             return _udp_manager.on_new_initial_context().then([this]() mutable {
                 auto ic = _udp_manager.get_initial_context();
 
-                auto cb_af = ic.get_cb_async_flow();
-
-                auto shell = new cb_async_flow_shell(ic.get_cb_async_flow());
-                shell->register_event();
-                shell->register_cb();
+                auto runner = new firewall_runner(ic.get_cb_async_flow(), (*this));
+                runner->events_registration();
+                runner->run_firewall();
 
                 return stop_iteration::no;
             });
@@ -254,29 +442,45 @@ public:
         uint64_t egress_send;
         unsigned egress_failed_send;
         size_t active_flow_num;
+
+        unsigned mica_timeout_error;
+        unsigned insufficient_mica_rd_erorr;
+
         void operator+=(const info& o) {
             ingress_received += o.ingress_received;
             egress_send += o.egress_send;
             egress_failed_send += o.egress_failed_send;
             active_flow_num += o.active_flow_num;
+            mica_timeout_error += o.mica_timeout_error;
+            insufficient_mica_rd_erorr += o.insufficient_mica_rd_erorr;
         }
     };
-    info _old{0,0,0,0};
+    info _old{0,0,0,0,0,0};
+    unsigned _mica_timeout_error = 0;
+    unsigned _insufficient_mica_rd_erorr=0;
 
     future<info> get_info() {
-        return make_ready_future<info>(info{_ingress_port.get_qp_wrapper().rx_pkts(),
+        /*return make_ready_future<info>(info{_ingress_port.get_qp_wrapper().rx_pkts(),
                                             _egress_port.get_qp_wrapper().tx_pkts(),
                                             _egress_port.peek_failed_send_cout(),
-                                            _udp_manager.peek_active_flow_num()});
+                                            _udp_manager.peek_active_flow_num()});*/
+        return make_ready_future<info>(info{_ingress_port.get_qp_wrapper().rx_pkts(),
+                                            _ingress_port.get_qp_wrapper().tx_pkts(),
+                                            _ingress_port.peek_failed_send_cout(),
+                                            _udp_manager.peek_active_flow_num(),
+                                            _mica_timeout_error,
+                                            _insufficient_mica_rd_erorr});
     }
     void collect_stats(int) {
         repeat([this]{
             return forwarders.map_reduce(adder<info>(), &forwarder::get_info).then([this](info i){
-                fprint(std::cout, "ingress_received=%d, egress_send=%d, egress_failed_send=%d, active_flow_num=%d.\n",
+                fprint(std::cout, "ingress_received=%d, egress_send=%d, egress_failed_send=%d, active_flow_num=%d, mica_timeout_error=%d, insufficient_mica_rd_erorr=%d.\n",
                         i.ingress_received-_old.ingress_received,
                         i.egress_send - _old.egress_send,
                         i.egress_failed_send - _old.egress_failed_send,
-                        i.active_flow_num);
+                        i.active_flow_num,
+                        i.mica_timeout_error - _old.mica_timeout_error,
+                        i.insufficient_mica_rd_erorr - _old.insufficient_mica_rd_erorr);
                 _old = i;
             }).then([]{
                 return seastar::sleep(1s).then([]{
@@ -285,19 +489,45 @@ public:
             });
         });
     }
+public:
+    Firewall firewall;
 };
 
 int main(int ac, char** av) {
     app_template app;
     ports_env all_ports;
+    per_core_objs<mica_client> mica_clients;
+    vector<vector<port_pair>> queue_map;
 
-    return app.run_deprecated(ac, av, [&app, &all_ports] {
+    return app.run_deprecated(ac, av, [&app, &all_ports, &mica_clients, &queue_map] {
         auto& opts = app.configuration();
-        return all_ports.add_port(opts, 0, smp::count, port_type::original).then([&opts, &all_ports]{
-            return all_ports.add_port(opts, 1, smp::count, port_type::original);
-        }).then([&all_ports]{
-            return forwarders.start(std::ref(all_ports));
+        return all_ports.add_port(opts, 0, smp::count, port_type::netstar_dpdk).then([&opts, &all_ports]{
+            return all_ports.add_port(opts, 1, smp::count, port_type::fdir);
+        }).then([&mica_clients]{
+           return mica_clients.start(&mica_clients);
+        }).then([&all_ports, &mica_clients]{
+            return mica_clients.invoke_on_all([&all_ports](mica_client& mc){
+                mc.configure_ports(all_ports, 1, 1);
+            });
+        }).then([&opts, &all_ports, &queue_map]{
+            queue_map = calculate_queue_mapping(opts, all_ports.local_port(1));
+        }).then([&mica_clients, &opts, &queue_map]{
+            return mica_clients.invoke_on_all([&opts, &queue_map](mica_client& mc){
+                mc.bootup(opts, queue_map);
+            });
+        }).then([&mica_clients]{
+            return mica_clients.invoke_on_all([](mica_client& mc){
+                mc.start_receiving();
+            });
+        }).then([&all_ports, &mica_clients]{
+            return forwarders.start(std::ref(all_ports), std::ref(mica_clients));
+        })/*.then([]{
+            return forwarders.invoke_on_all(&forwarder::mica_test, 1);
         }).then([]{
+            return forwarders.invoke_on_all(&forwarder::mica_test, 1);
+        }).then([]{
+            return forwarders.invoke_on_all(&forwarder::mica_test, 1);
+        })*/.then([]{
             return forwarders.invoke_on_all(&forwarder::configure, 1);
         }).then([]{
             return forwarders.invoke_on_all(&forwarder::run_udp_manager, 1);
@@ -305,9 +535,22 @@ int main(int ac, char** av) {
             return forwarders.invoke_on(0, &forwarder::collect_stats, 1);
         }).then([]{
             fprint(std::cout, "forwarder runs!\n");
-            /*engine().at_exit([]{
-                return forwarders->stop();
-            });*/
+            engine().at_exit([]{
+                return forwarders.stop();
+            });
         });
     });
 }
+
+// 1 thread forwarder, sender use static udp traffic gen, 700000 total pps, 1000 flows
+// The system barely crashes, which is good.
+
+// When turn on mica debug mode, mica will fail to made an assertion about memory alignment in the packet
+// This is easy to reproduce even when 1 flow with 1pps. Please check this out carefully!
+
+// 1r: 8.5M.
+// 1r1w: 5.35M
+// 2r2w: 3.4M
+// 3r3w: 2.43M
+
+
