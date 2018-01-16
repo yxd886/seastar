0303f2be082e5d1ce1d09bca38f9bce71fe88082
duanjp8617 <duanjp8617@gmail.com>
2018-01-16 23:01:02 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -1,4 +1,3 @@
-#include "netstar/mica/util/hash.h"
 #include <rte_ethdev.h>
 #include <experimental/optional>
 #include <stdio.h>
@@ -7,10 +6,5 @@
 
 int main(){
     netstar::rte_packet pkt();
-    int num = 512;
-    auto hash = mica::util::hash_cityhash(&num, sizeof(num));
-    std::experimental::optional<rte_mbuf*> tester;
-    printf("The size of std::experimental::optional<rte_mbuf*> is %zu\n", sizeof( std::experimental::optional<rte_mbuf*>));
-    printf("The size of rte_mbuf* is %zu\n", sizeof(rte_mbuf*));
     return hash;
 }
