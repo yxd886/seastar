bb61344842eeb209609effc87b481b782ae766a9
duanjp8617 <duanjp8617@gmail.com>
2018-01-16 21:05:29 +0800

    sync

--- configure.py
+++ configure.py
@@ -336,7 +336,6 @@ libnet = [
     
 libnetstar = [
     'netstar/mica/util/cityhash/city_mod.cc',
-    'netstar/device/base.cc',
     'netstar/device/fdir_device.cc',
     'netstar/device/standard_device.cc',
     ]
