b835e7313475fdf689546718104d3fa8cb71eae4
duanjp8617 <duanjp8617@gmail.com>
2018-01-16 16:12:03 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -7,6 +7,7 @@ int main(){
     int num = 512;
     auto hash = mica::util::hash_cityhash(&num, sizeof(num));
     std::experimental::optional<rte_mbuf*> tester;
-    printf("The size of std::experimental::optional<rte_mbuf*> is %zu\n", sizeof(tester));
+    printf("The size of std::experimental::optional<rte_mbuf*> is %zu\n", sizeof( std::experimental::optional<rte_mbuf*>));
+    printf("The size of rte_mbuf* is %zu\n", sizeof(rte_mbuf*));
     return hash;
 }
