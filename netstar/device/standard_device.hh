388abcfe177f1df85efbc109d9a7c67ce4b48a32
duanjp8617 <duanjp8617@gmail.com>
2018-01-16 22:58:14 +0800

    sync

---
+++ netstar/device/standard_device.hh
@@ -0,0 +1,25 @@
+/*
+ * netstar
+ */
+
+#ifdef HAVE_DPDK
+
+#ifndef _STANDARD_DEVICE_HH
+#define _STANDARD_DEVICE_HH
+
+#include <memory>
+#include "net/net.hh"
+#include "core/sstring.hh"
+
+namespace netstar{
+
+std::unique_ptr<seastar::net::device> create_standard_device(
+                                    uint8_t port_idx = 0,
+                                    uint8_t num_queues = 4,
+                                    bool use_lro = false,
+                                    bool enable_fc = false);
+} // namespace netstar
+
+#endif // _STANDARD_DEVICE_HH
+
+#endif // HAVE_DPDK
