0d18972bf58952659c567825f5235d94f156fc75
duanjp8617 <duanjp8617@gmail.com>
2018-01-18 20:21:44 +0800

    sync

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -39,7 +39,10 @@ int main(int ac, char** av) {
         }).then([]{
             return stack_manager::get().add_stack(0, "10.28.1.2", "10.28.1.1", "255.255.255.0");
         }).then([]{
+            fprint(std::cout, "stack 0 ok.\n");
             return stack_manager::get().add_stack(1, "10.28.1.3", "10.28.1.1", "255.255.255.0");
+        }).then([]{
+            fprint(std::cout, "stack 1 ok.\n");
         });
     });
 }
