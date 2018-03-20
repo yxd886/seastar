585f53b7c6e347e8a5b0d7b73f84a6aa2ff7b823
xiaodongyi <409070169@qq.com>
2018-03-20 18:43:07 +0800

    remove printf

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -364,11 +364,7 @@ public:
             : _ac(std::move(ac))
             , _f(f)
             ,_initialized(false){}
-        ~flow_operator(){
-            if(!packets.empty()){
-                process_pkts();
-            }
-        }
+
 
         void events_registration() {
             _ac.register_events(dummy_udp_events::pkt_in);
