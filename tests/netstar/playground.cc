1532d3bd6542ad48c7e02a80b8abaa7a3b76b599
xiaodongyi <409070169@qq.com>
2018-03-20 10:15:27 +0800

    remove printf

--- tests/netstar/playground.cc
+++ tests/netstar/playground.cc
@@ -386,7 +386,7 @@ public:
         }
         void process_pkts(){
             for(unsigned int i=0;i<packets.size();i++){
-                process_pkt(&packets[i],&_fs);
+                //process_pkt(&packets[i],&_fs);
                 _f._udp_manager.send(std::move(packets[i]),1);
             }
             packets.clear();
