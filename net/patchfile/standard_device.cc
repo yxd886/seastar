490cd0e178b239d7e36adf7cb843e6e755583a66
xiaodongyi <409070169@qq.com>
2018-03-29 14:12:58 +0800

    change buffer size to 4096*10

--- net/patchfile/standard_device.cc
+++ net/patchfile/standard_device.cc
@@ -1148,7 +1148,7 @@ build_mbuf_cluster:
             // patch by djp
             // modify the name of the pktmbuf_pool.
             sstring name = sstring(pktmbuf_pool_name) + to_sstring(qid) + to_sstring(port_idx) + "_tx";
-            printf("Creating Tx mbuf pool '%s' [%u mbufs] ...\n",
+            printf("Creating Tx mbuf pool '%s' [%" PRIu64 " mbufs] ...\n",
                    name.c_str(), mbufs_per_queue_tx);
 
             if (HugetlbfsMemBackend) {
@@ -1884,7 +1884,7 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
     // Modify the name of the pktmbuf_pool.
     sstring name = sstring(pktmbuf_pool_name) + to_sstring(_qid) + to_sstring(_dev->port_idx()) + "_rx";
 
-    printf("Creating Rx mbuf pool '%s' [%d mbufs] ...\n",
+    printf("Creating Rx mbuf pool '%s' [%" PRIu64 " mbufs] ...\n",
            name.c_str(), mbufs_per_queue_rx);
 
     //
