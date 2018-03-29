b4119dd22c9352ca33a852c20e455006770d3b04
xiaodongyi <409070169@qq.com>
2018-03-29 12:43:58 +0800

    change snort_dfa_patterns

--- nf/aho-corasick/aho.h
+++ nf/aho-corasick/aho.h
@@ -24,12 +24,12 @@
 #define AHO_MAX_PATTERN_LEN (1024)
 #define AHO_MAX_THREADS 28
 
-#define AHO_MAX_DFA 250
+#define AHO_MAX_DFA 200
 /*
  * DFAs patterns captured with the following configuration:
  * snort 2.9.7, conf: config detection: search-method ac-q
  */
-#define AHO_PATTERN_FILE "/home/net/async-nf/seastar/nf/aho-corasick/snort_dfa_pattern"
+#define AHO_PATTERN_FILE "/home/net/async-nf/seastar/nf/aho-corasick/snort_dfa_patterns"
 
 #define AHO_PACKET_FILE "../../../data_dump/snort/snort_packets"
 #define AHO_MAX_PKTS (128 * 1024)	/* Reading 2M packets takes a long time */
